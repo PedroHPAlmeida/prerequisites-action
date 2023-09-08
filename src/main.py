92856975843cac61b8eae3948a220db7d6759d00
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-09-07 21:47:47 -0300

    fix: corrige chamada da função passando todos os parâmetros necessários

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ print(f'Creating branchs: {branches}')
 github.create_branchs(repo_owner, repo_name, branches, default_branch)
 
 print('Defining branch protection')
-github.protect_branchs([default_branch, *branches])
+github.protect_branchs(repo_owner, repo_name, [default_branch, *branches])
 
 print('Defining application name to be used in other actions')
 github.create_repository_variable(repo_owner, repo_name, 'APP_NAME', repo_name)
